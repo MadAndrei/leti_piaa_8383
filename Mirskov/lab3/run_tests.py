6cafeb1edbb91a94648653f2ac1ec82c3a61a040
MadAndrei <andreimirskov@yandex.ru>
2020-05-09 12:27:56 +0300

    delete lab3

--- Mirskov/lab3/run_tests.py
+++
@@ -1,6 +0,0 @@
-import os
-for test_num, test_name in enumerate(os.listdir('Tests')):
-	print('Run test number', test_num)
-	print(''.join([line for line in open('Tests/' + test_name, 'r')]))
-	os.system('./lab3 < ./Tests/' + test_name)
-	print()
